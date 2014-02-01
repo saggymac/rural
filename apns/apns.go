dcd82b08188da6eef9a6f780bf0cf1f3f4a76680
Scott A. Guyer <saguyer@gmail.com>
2014-02-01 00:06:36 -0500

    wip building out apns

--- apns/apns.go
+++ apns/apns.go
@@ -1,12 +1,49 @@
 package apns
 
+import {
+	"net/http"
+}
+
+const (
+	DELIVERY_IMMEDIATE = iota
+	DELIVERY_DEFERRED
+)
 
 
-func RegisterDevice () {
+
+type PushItem struct {
+
+}
+
+type PushMessage struct {
+	// 32 bytes
+	string DeviceToken
+
+	// up to 256 bytes of json
+	string Payload
+
+	// Ignoring expiration, 4 bytes
+
+	// Use the DELIVERY_* package constants, 1 byte
+	int DeliveryMethod
+} 
+
+
+func RegisterDevice (  ) {
 
 }
 
 
-func SendPushMessage () {
+//
+// TODO: going to need to figure out how to configure the Cert for connecting to 
+// APNS.
+//
 
+//
+// Apple docs suggest that you should keep the TCP connection open while sending as 
+// many messages as you like. So we should build a session concept into this package.
+// In these initial implementations, it's connect, send, close.
+//
+func SendPushMessage ( m *PushMessage ) {
+	// TODO: 
 }
